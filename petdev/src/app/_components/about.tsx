24bb5f39da85efc68564ab7d163438ab4de40e22
Magnus Oss <magnusoss98@gmail.com>
2025-04-05 23:16:10 -0300

    Atualizar o about.tsx

--- petdev/src/app/_components/about.tsx
+++ petdev/src/app/_components/about.tsx
@@ -66,7 +66,7 @@ export function About() {
                 Contato via WhatsApp
               </a>
               <a
-                href="#"
+                href="https://maps.app.goo.gl/FXiygtufNj1L69XU9"
                 className="flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
               >
                 <MapPin className="w-5 h-5 text-black" />
